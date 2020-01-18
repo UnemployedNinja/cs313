102add12d8af7438962996e7e31c5183dea29b86
unknown <phi13025@byui.edu>
2020-01-17 19:14:26 -0700

    test

--- web/asignments/week2/email.php
+++ web/asignments/week2/email.php
@@ -1,19 +1,17 @@
 <?php 
 
 // https://stackoverflow.com/questions/18379238/send-email-with-php-from-html-form-on-submit-with-the-same-script
-    if(isset($_POST['submit'])) {
+   
 
-        $myEmail = "phi13025@byui.edu";      // My email
-        $name = $_POST["name"];          // Sender name
-        $email = $_POST["email"];        // Sender email
-        $comment = $_POST["comment"];    // Sender comments
-        $subject = "Assignment Homepage";
+    $myEmail = "phi13025@byui.edu";      // My email
+    $name = $_POST["name"];          // Sender name
+    $email = $_POST["email"];        // Sender email
+    $comment = $_POST["comment"];    // Sender comments
+    $subject = "Assignment Homepage";
 
-        // mail($myEmail,$subject,$comment,$email);
-        // mail($email,$subject,$name,$myEmail); // Send copy to sender
-        echo "Email was sent. Thank you " . $name;
+    // mail($myEmail,$subject,$comment,$email);
+    // mail($email,$subject,$name,$myEmail); // Send copy to sender
 
-    }
     echo "And email was sent Shawn. Thank you " . $name;
 
    // header("Location: Assignment Homepage.html");
