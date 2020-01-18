875bce60a18b65f531702725b9228ea3385ddbff
unknown <phi13025@byui.edu>
2020-01-17 19:11:26 -0700

    test

--- web/asignments/week2/email.php
+++ web/asignments/week2/email.php
@@ -1,7 +1,7 @@
 <?php 
 
 // https://stackoverflow.com/questions/18379238/send-email-with-php-from-html-form-on-submit-with-the-same-script
-echo "Email was sent. Thank you " . $name;
+echo "Email was sent Shawn. Thank you " . $name;
     if(isset($_POST['submit'])) {
 
         $myEmail = "phi13025@byui.edu";      // My email
@@ -15,8 +15,6 @@ echo "Email was sent. Thank you " . $name;
         echo "Email was sent. Thank you " . $name;
 
     }
-    echo "Email was sent. Thank you " . $name;
-
 
    // header("Location: Assignment Homepage.html");
         
