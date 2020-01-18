00f71c2b797b871cf40431667afc63100caf54d5
unknown <phi13025@byui.edu>
2020-01-17 20:10:48 -0700

    test

--- web/asignments/week2/email.php
+++ web/asignments/week2/email.php
@@ -9,14 +9,15 @@ if($_POST["name"] != "" && $_POST["email"] != "" && $_POST["comment"] != "") {
     $email = $_POST["email"];        // Sender email
     $comment = $_POST["comment"];    // Sender comments
     $subject = "Assignment Homepage HTML email";
+    $message = $name . " Wrote the following: " . "\n\n" . $comment;
 
     // $headers = "MIME-Version: 1.0" . "\r\n";
     // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
     // $headers .= "From: " . $email . "\r\n";
 
-    $headers = "From: HTML " . "\r\n" . "CC: " . $email;
+    $headers = "From:" . $email;
 
-    mail($to,$subject,$comment,$headers);
+    mail($to,$subject,$message,$headers);
     // mail($email,$subject,$name,$myEmail); // Send copy to sender
 
     echo "And email was sent Shawn. Thank you for your feedback " . $name;
