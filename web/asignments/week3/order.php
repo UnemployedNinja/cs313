8d504f6ff86478e4e3bd52d24cae6dbb7130ba0c
unknown <phi13025@byui.edu>
2020-01-24 18:44:07 -0700

    session

--- web/asignments/week3/order.php
+++ web/asignments/week3/order.php
@@ -19,25 +19,18 @@
 
 
  if(!empty($_POST['season'])) {
-        // echo "Your items are: <br>";
-        $totalCost = 0;
-        $vector = new \Ds\Vector();
 
+        $totalCost = 0;
 
         foreach($_POST['season'] as $value) {
             if($value == 0) {
-                $_SESSION['pictures'] = "https://www.google.com/search?biw=798&bih=751&tbm=isch&sxsrf=ACYBGNS7DMvY3yoggt7dH2TkTpLUqQyEvA%3A1579641632941&sa=1&ei=IGsnXsvmOJC1tAaPtLT4CA&q=tmnt+classic+cartoons+season+1&oq=tmnt+classic+cartoons+season+1&gs_l=img.3..35i39.217382.217382..217806...0.0..0.77.213.3......0....1..gws-wiz-img.yKIUL_Yyu7k&ved=0ahUKEwjL9pzTz5XnAhWQGs0KHQ8aDY8Q4dUDCAc&uact=5#imgrc=tqUHZy_HU17MWM:";
+                print '<img src="season1.jpg"> />';
                 $cost = "6.98";
                 $totalCost += $cost;
-                $vector->push($cost);
             }
         }
     } else {
         header("url=browse.php");
-    }
-    
-    foreach($_SESSION as $img) {
-        echo 'Your items are: ' . $_SESSION['pictures'] . ' Cost: ' . $vector . '<br>';
     }
 
     echo "Your total cost is $totalCost <br><br>";
