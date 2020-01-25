921812d46a2f5755200190906856afc872e3c41b
unknown <phi13025@byui.edu>
2020-01-25 16:59:06 -0700

    remove button

--- web/asignments/week3/order.php
+++ web/asignments/week3/order.php
@@ -27,12 +27,12 @@
                 echo '<img src="season1.jpg" width="188" height="268">';
                 $cost = "6.98";
                 echo "Price: $cost";
-                echo '<input type="submit" class="button" name=remove" value="Remove" />';
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
                 echo "<br><hr>";
                 $totalCost += $cost;
-                if(isset($_Post['remove'])) {
+                if(isset($_POST['remove'])) {
                     unset($_POST['season']);
-                    $totalCost -=$cost;
+                    $totalCost -= $cost;
                     header("Refresh:0");
                 }
 
@@ -41,55 +41,73 @@
             if($value == 2) {
                 echo '<img src="season2.jpg" width="188" height="268">';
                 $cost = "6.39";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 3) {
                 echo '<img src="season3.jpg" width="188" height="268">';
                 $cost = "9.15";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 4) {
                 echo '<img src="season4.jpg" width="188" height="268">';
                 $cost = "6.35";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 5) {
                 echo '<img src="season5.jpg" width="188" height="268">';
                 $cost = "8.96";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 6) {
                 echo '<img src="season6.jpg" width="188" height="268">';
                 $cost = "10.04";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 7) {
                 echo '<img src="season7.jpg" width="188" height="268">';
                 $cost = "13.89";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 8) {
                 echo '<img src="season8.jpg" width="188" height="268">';
                 $cost = "6.88";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 9) {
                 echo '<img src="season9.jpg" width="188" height="268">';
                 $cost = "6.39";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
             if($value == 10) {
                 echo '<img src="season10.jpg" width="188" height="268">';
                 $cost = "7.49";
-                echo "Price: $cost <br><hr>";
+                echo "Price: $cost";
+                echo '<input type="submit" class="button" name="remove" value="Remove" />';
+                echo "<br><hr>";
                 $totalCost += $cost;
             }
         }
