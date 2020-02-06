5f8b88f795894d050ded73f2c2fdf08fa64492d6
unknown <phi13025@byui.edu>
2020-02-05 18:57:58 -0700

    added login page and am editing api usage

--- web/project1/home.php
+++ web/project1/home.php
@@ -13,35 +13,43 @@
         <a href="#about">About</a>
         <a href="#contact">Contact</a>
             <div class="search-container">
-                <form action="/action_page.php" method="GET">
+                <form action="" method="GET">
                     <input type="text" placeholder="Search.." name="search">
-                    <button type="submit">Submit</button>
+                    <button type="submit" name="submit">Submit</button>
                 </form>
             </div>
     </div>
 
         <?php
 
-            $client = new http\Client;
-            $request = new http\Client\Request;
-            
-            $request->setRequestUrl('https://movie-database-imdb-alternative.p.rapidapi.com/');
-            $request->setRequestMethod('GET');
-            $request->setQuery(new http\QueryString(array(
-                'page' => '1',
-                'r' => 'json',
-                's' => 'Avengers Endgame' // $_GET(search);
-            )));
-            
-            $request->setHeaders(array(
-                'x-rapidapi-host' => 'movie-database-imdb-alternative.p.rapidapi.com',
-                'x-rapidapi-key' => 'SIGN-UP-FOR-KEY'
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+	            CURLOPT_URL => "https://movie-database-imdb-alternative.p.rapidapi.com/?page=1&r=json&s=Avengers%20Endgame",
+	            CURLOPT_RETURNTRANSFER => true,
+	            CURLOPT_FOLLOWLOCATION => true,
+	            CURLOPT_ENCODING => "",
+	            CURLOPT_MAXREDIRS => 10,
+	            CURLOPT_TIMEOUT => 30,
+	            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	            CURLOPT_CUSTOMREQUEST => "GET",
+	            CURLOPT_HTTPHEADER => array(
+		            "x-rapidapi-host: movie-database-imdb-alternative.p.rapidapi.com",
+		            "x-rapidapi-key: SIGN-UP-FOR-KEY"
+	            ),
             ));
-            
-            $client->enqueue($request)->send();
-            $response = $client->getResponse();
-            
-            echo $response->getBody();
+
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+
+            curl_close($curl);
+
+            if ($err) {
+	            echo "cURL Error #:" . $err;
+            } else {
+	            echo $response;
+            }
+
     
         ?>
 
