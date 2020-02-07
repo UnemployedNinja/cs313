254fc340f5aca6d61d0870619d9f2683e1ffd618
unknown <phi13025@byui.edu>
2020-02-07 12:46:31 -0700

    test

--- web/project1/home.php
+++ web/project1/home.php
@@ -33,11 +33,20 @@
 
         <?php
             
-            include_once "api/api_popular.php";
+            include "info.php";
+            $title="Popular Movies";
+            include_once "header.php";
 
+            include_once "api/api_popular.php";
             foreach($popular->results as $p){
                 echo '<li><a href="movie.php?id=' . $p->id . '"><img src="'.$imgurl_1.''. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4><h5><em> Rate : " . $p->vote_average . " | Vote : " . $p->vote_count . " | Popularity : " . round($p->popularity) . "</em></h5></a></li>";
             }
+
+            // include_once "api/api_popular.php";
+
+            // foreach($popular->results as $p){
+            //     echo '<li><a href="movie.php?id=' . $p->id . '"><img src="'.$imgurl_1.''. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4><h5><em> Rate : " . $p->vote_average . " | Vote : " . $p->vote_count . " | Popularity : " . round($p->popularity) . "</em></h5></a></li>";
+            // }
            
             // } else {
 
---
+++ web/project1/info.php
@@ -0,0 +1,11 @@
+<?php
+
+$apikey = "ef7339152eff424955433540cf591fc4";
+
+$sitename = "Update Film";
+$tagline = "Info Film Update";
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
