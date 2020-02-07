e88efb1a41f0375b8975a4645e0a23b25d58e31a
unknown <phi13025@byui.edu>
2020-02-07 16:06:17 -0700

    test search

--- web/project1/home.php
+++ web/project1/home.php
@@ -46,79 +46,79 @@
             }   
 
             // Searching for specific data 
-            if($_GET['submit']) {
+            // if($_GET['submit']) {
 
-                $input = $_GET['search'];
-                $channel = $_GET['channel'];
-                $search = $input;
+            //     $input = $_GET['search'];
+            //     $channel = $_GET['channel'];
+            //     $search = $input;
 
-                include_once "conf/info.php";
-                $title = 'Result Search | '.$input;
+            //     include_once "conf/info.php";
+            //     $title = 'Result Search | '.$input;
 
-                include_once "api/api_search.php";
+            //     include_once "api/api_search.php";
 
-                if($channel=="movie"){	
+            //     if($channel=="movie"){	
 
-                    foreach($search->results as $results){
+            //         foreach($search->results as $results){
 
-			            $title = $results->title;
-			            $id = $results->id;
-                        $releas= $results->release_date;
+			//             $title = $results->title;
+			//             $id = $results->id;
+            //             $releas= $results->release_date;
                         
-			            if (!empty($release) && !is_null($release)){
+			//             if (!empty($release) && !is_null($release)){
 
-				            $tempyear = explode("-", $release);
-                            $year = $tempyear[0];
+			// 	            $tempyear = explode("-", $release);
+            //                 $year = $tempyear[0];
                             
-				            if (!is_null($year)){
-					            $title = $title.' ('.$year.')';
-				            }
-                        }
+			// 	            if (!is_null($year)){
+			// 		            $title = $title.' ('.$year.')';
+			// 	            }
+            //             }
                         
-                        $backdrop 	= $results->backdrop_path;
+            //             $backdrop 	= $results->backdrop_path;
                         
-			            if (empty($backdrop) && is_null($backdrop)){
+			//             if (empty($backdrop) && is_null($backdrop)){
 
-                            $backdrop =  dirname($_SERVER['PHP_SELF']).'/image/no-gambar.jpg';
+            //                 $backdrop =  dirname($_SERVER['PHP_SELF']).'/image/no-gambar.jpg';
                             
-			            } else {
+			//             } else {
 
-                            $backdrop = 'http://image.tmdb.org/t/p/w300'.$backdrop;
+            //                 $backdrop = 'http://image.tmdb.org/t/p/w300'.$backdrop;
                             
-			            }
-			            echo '<li><a href="movie.php?id=' . $id . '"><img src="'.$backdrop.'"><h4>'.$title.'</h4></a></li>';
-                    }
+			//             }
+			//             echo '<li><a href="movie.php?id=' . $id . '"><img src="'.$backdrop.'"><h4>'.$title.'</h4></a></li>';
+            //         }
                     
-                }elseif($channel=="tv"){
+            //     }elseif($channel=="tv"){
 
-                    foreach($search->results as $results){
+            //         foreach($search->results as $results){
 
-			            $title 		= $results->original_name;
-			            $id 		= $results->id;
-                        $release	= $results->first_air_date;
+			//             $title 		= $results->original_name;
+			//             $id 		= $results->id;
+            //             $release	= $results->first_air_date;
                         
-			            if (!empty($release) && !is_null($release)){
+			//             if (!empty($release) && !is_null($release)){
 
-				            $tempyear 	= explode("-", $release);
-                            $year 		= $tempyear[0];
+			// 	            $tempyear 	= explode("-", $release);
+            //                 $year 		= $tempyear[0];
                             
-				            if (!is_null($year)){
-					            $title = $title.' ('.$year.')';
-				            }
-                        }   
+			// 	            if (!is_null($year)){
+			// 		            $title = $title.' ('.$year.')';
+			// 	            }
+            //             }   
                         
-                        $backdrop 	= $results->backdrop_path;
+            //             $backdrop 	= $results->backdrop_path;
                         
-			            if (empty($backdrop) && is_null($backdrop)){
+			//             if (empty($backdrop) && is_null($backdrop)){
 
-                            $backdrop = $pathloc.'image/no-backdrop.png';
+            //                 $backdrop = $pathloc.'image/no-backdrop.png';
                             
-			            } else {
-				            $backdrop = 'http://image.tmdb.org/t/p/w300'.$backdrop;
-			            }
-			            echo '<li><a href="tvshow.php?id=' . $id . '"><img src="'.$backdrop.'"><h4>'.$title.'</h4></a></li>';
-		            }
-                }
+			//             } else {
+			// 	            $backdrop = 'http://image.tmdb.org/t/p/w300'.$backdrop;
+			//             }
+			//             echo '<li><a href="tvshow.php?id=' . $id . '"><img src="'.$backdrop.'"><h4>'.$title.'</h4></a></li>';
+		    //         }
+            //     }
         ?>
 
     </body>
