f8af1430f5d5af449b651288d93a30ef32ebaf31
unknown <phi13025@byui.edu>
2020-02-07 19:07:54 -0700

    db displays

--- web/project1/mylist.php
+++ web/project1/mylist.php
@@ -1,18 +1,18 @@
-<!-- <?php
+<?php
 
-   require "dbConnect.php";
+   // require "dbConnect.php";
 
-   $db = get_db();
-   $movie = $db->prepare("SELECT * FROM moive");
-   $movie->execute();
-   while ($row = $movie->fetch(PDO::FETCH_ASSOC))
-   {
-      $title = $row["title"];
-      $year = $row["year"];
-      $image = $row["cover"];
-      echo "$title<br>$year<br><img src='$image'>";
+   // $db = get_db();
+   // $movie = $db->prepare("SELECT * FROM moive");
+   // $movie->execute();
+   // while ($row = $movie->fetch(PDO::FETCH_ASSOC))
+   // {
+   //    $title = $row["title"];
+   //    $year = $row["year"];
+   //    $image = $row["cover"];
+   //    echo "$title<br>$year<br><img src='$image'>";
    }
-?> -->
+?>
 
 <?php
    require "dbConnect.php";
