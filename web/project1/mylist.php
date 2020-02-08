79c3b7532c10a51e4a4f3a9285d68a01a625b799
unknown <phi13025@byui.edu>
2020-02-07 18:58:14 -0700

    db displays

--- web/project1/mylist.php
+++ web/project1/mylist.php
@@ -1,15 +1,14 @@
 <?php
 
-   include “dbConnect.php”;
-   $db = get_db();
+   require "dbConnect.php";
 
-   $movie = $db->prepare('SELECT * FROM movie'); 
-   $movie->execute();
-   while ($row = $movie->fetch(PDO::FETCH_ASSOC))
+   $db = get_db();
+   $w5_event = $db->prepare("SELECT * FROM w5_event");
+   $w5_event->execute();
+   while ($row = $w5_event->fetch(PDO::FETCH_ASSOC))
    {
-      echo "in while";
-      $name = $row[“name”];
-      $image = $row[“image”];
-      echo "$name<br><img src=‘$image’>";
+      $name = $row["name"];
+      $image = $row["image"];
+      echo "$name<br><img src='$image'>";
    }
 ?>
