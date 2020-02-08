7bedc888b3db3cd8ce03499661fcc0742be1b803
unknown <phi13025@byui.edu>
2020-02-07 19:10:29 -0700

    db displays

--- web/project1/mylist.php
+++ web/project1/mylist.php
@@ -1,31 +1,32 @@
 <?php
-
    // require "dbConnect.php";
 
    // $db = get_db();
-   // $movie = $db->prepare("SELECT * FROM moive");
-   // $movie->execute();
-   // while ($row = $movie->fetch(PDO::FETCH_ASSOC))
+
+   // $w5_event = $db->prepare("SELECT * FROM w5_event");
+   // $w5_event->execute();
+
+   // while ($row = $w5_event->fetch(PDO::FETCH_ASSOC))
    // {
-   //    $title = $row["title"];
-   //    $year = $row["year"];
-   //    $image = $row["cover"];
-   //    echo "$title<br>$year<br><img src='$image'>";
+   //    $name = $row["name"];
+   //    $image = $row["image"];
+   //    echo "$name<br><img src='$image'>";
    // }
 ?>
 
 <?php
+
    require "dbConnect.php";
 
    $db = get_db();
-
-   $w5_event = $db->prepare("SELECT * FROM w5_event");
-   $w5_event->execute();
-
-   while ($row = $w5_event->fetch(PDO::FETCH_ASSOC))
+   $movie = $db->prepare("SELECT * FROM movie");
+   $movie->execute();
+   while ($row = $movie->fetch(PDO::FETCH_ASSOC))
    {
-      $name = $row["name"];
-      $image = $row["image"];
-      echo "$name<br><img src='$image'>";
+      $title = $row["title"];
+      $year = $row["year"];
+      $image = $row["cover"];
+      echo "$title<br>$year<br><img src='$image'>";
    }
 ?>
+
