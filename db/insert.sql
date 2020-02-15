ade2d7a3fc8f95824c02ea532222d97b36c6bd68
unknown <phi13025@byui.edu>
2020-02-14 17:11:21 -0700

    sessions

--- db/insert.sql
+++ db/insert.sql
@@ -22,11 +22,23 @@ INSERT INTO movie ( title
                   , '1994'
                   , 'https://www.google.com/search?q=tmnt+season+1&tbm=isch&sxsrf=ACYBGNTfgb0gI5MxpND6PQ9h8y7IsvP4Vw:1581128730233&source=lnms&sa=X&ved=0ahUKEwj32rzC88DnAhViOX0KHXAMAhcQ_AUIswIoAQ&biw=1280&bih=885#imgrc=tqUHZy_HU17MWM');
 
+INSERT INTO movie ( title
+                  , year
+                  , cover) 
+           VALUES ( 'Anime'
+                  , '2003'
+                  , 'https://www.google.com/search?q=tmnt+season+1&tbm=isch&sxsrf=ACYBGNTfgb0gI5MxpND6PQ9h8y7IsvP4Vw:1581128730233&source=lnms&sa=X&ved=0ahUKEwj32rzC88DnAhViOX0KHXAMAhcQ_AUIswIoAQ&biw=1280&bih=885#imgrc=tqUHZy_HU17MWM');
+
 ---------- POPULATE THE list TABLE ----------
 INSERT INTO list ( user_id
                         , movie_id) 
                  VALUES ( '1'
                         , '1');
+
+INSERT INTO list ( user_id
+                        , movie_id) 
+                 VALUES ( '1'
+                        , '37');
 
 
 ---------- POPULATE THE episode_tracker TABLE ----------
