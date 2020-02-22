c0927e115bd3b14c3fb5f1ba312f3f536480fd32
unknown <phi13025@byui.edu>
2020-02-22 16:56:29 -0700

    Sessions

--- web/project1/authenticate.php
+++ web/project1/authenticate.php
@@ -23,7 +23,9 @@
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
             if (password_verify($password, $row["password"])) {
                 $authenticate = true;
-               // $_SESSION['UID'] = $db->prepare("SELECT id FROM user_profile WHERE username = :username");
+                $_SESSION['UID'] = $db->prepare("SELECT id FROM user_profile WHERE username = :username");
+                $statement->bindValue(':username', $username);
+                $statement->execute();
             
             }
         }
