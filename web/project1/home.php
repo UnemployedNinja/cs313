a753fb7757b017304885c8aaff95353130c2f744
unknown <phi13025@byui.edu>
2020-02-22 13:31:23 -0700

    Login complete

--- web/project1/home.php
+++ web/project1/home.php
@@ -17,7 +17,7 @@
         <a href="login.php">Login</a>
         <a>
             <?php
-                print_r($_SESSION['username']);
+                echo 'Session username is: ' . $_SESSION['username'];
             ?>
         </a>
 
